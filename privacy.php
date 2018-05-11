117961f1b2710ca3626ba9ffda38223566730675
dmora01 <dmora@csumb.edu>
2018-05-10 19:45:49 -0700

    Added privacy policy info

---
+++ privacy.php
@@ -0,0 +1,36 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Privacy Policy</title>
+
+		<link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <ul>
+			<li><a href = "index.php">Home</a></li>
+			<li><a href = "about.php">About</a></li>
+			<li><a href = "privacy.php">Privacy</a></li>
+		</ul>
+
+		<br><br><br>
+        
+        <h1>
+            Privacy Policy
+        </h1>
+
+        <br><br><br><br><br>
+
+        <p class = "description">
+            This developer has no access to your personal information, including your password used to login.
+            All login credentials are stored securely through Firebase, with passwords being witheld from the Firebase console.
+            Passwords are also <h6>not</h6> stored as plaintext, they are encypted when signing up and when changing your password.
+        </p>
+
+    </body>
+</html>
