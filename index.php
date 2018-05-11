1ba5798cfe4d92a7a6cade06b16279966e138f45
dmora01 <dmora@csumb.edu>
2018-05-10 19:15:10 -0700

    Added additional app

--- index.php
+++ index.php
@@ -1,38 +1,54 @@
-<?php 
+<?php
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		
+
 		<title></title>
-		
+
 		<link rel="stylesheet" href="css/styles.css">
 	</head>
 	<body>
 		<ul>
 			<li><a href = "index.php">Home</a></li>
 			<li><a href = "about.php">About</a></li>
+			<li><a href = "privacy.php">Privacy</a></li>
 		</ul>
-		
+
 		<br><br><br>
-		
+
 		<h1>
 			Apps
 		</h1>
-		
+
 		<br><br>
-		
+
 		<div class = "Translator">
 			<div class = "translatorAppIcon">
 				<img src = "img/translator.jpg" height = "200" width = "200">
-				<p class = "description">An iOS application that is able to translate over one hundred languages. Supported languages are able to be spoken in the language of your choice. You are able to type and say any sentence to be translated.
+				<p class = "description">An iOS application that is able to translate over one hundred languages.
+					Supported languages are able to be spoken in the language of your choice.
+					You are able to type and say any sentence to be translated.
 				</p>
 			</div>
 			<div class = "AppStore">
 				<br>
 				<a class = "link" href = "https://itunes.apple.com/us/app/translate-this-language/id1246949377?mt=8" target = "blank">App Store</a>
+			</div>
+		</div>
+		<div class = "RooMes">
+			<div class = "roomesAppIcon">
+				<img src = "img/roomies.png" height = "200" width = "200">
+				<p class = "description">An iOS application that gives you a central location for roommate affairs.
+					Discuss matters of the household without being out of the loop.
+					Sign up along with your roommates to get started.
+				</p>
+			</div>
+			<div class = "AppStore">
+				<br>
+				<a class = "link" href = "https://itunes.apple.com/us/app/roomes/id1323843003?mt=8" target = "blank">App Store</a>
 			</div>
 		</div>
 	</body>
