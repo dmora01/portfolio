7e4f9caa27dc27d06e30a430e1dd45a44b924d34
dmora01 <dmora@csumb.edu>
2019-10-02 09:18:27 -0700

    Fixed typo

--- support.php
+++ support.php
@@ -28,7 +28,7 @@
 
         <p class="description">
             Are any applications giving you trouble? Do you have any suggestions? Let me know which app and what version you are on.<br><br>
-            If there are any bugs or crashes that are interfereing with your experience please let me know.
+            If there are any bugs or crashes that are interfering with your experience please let me know.
             Write a detailed report on how to repeat it. <br><br>
             If you want to see something added or have general questions, shoot them way and I will take a look.<br><br>
             Send an email my way: dmora.dev.contact@gmail.com
